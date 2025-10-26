f6d4a3fb7ebd467304fb34686733d1449a54f88d
Milan <37807335+mailandr@users.noreply.github.com>
2025-10-26 19:21:00 +0100

    adjust pagination and swiper script

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 let swiper_elements = document.querySelectorAll(".js-swiper");
 
 if (swiper_elements !== null) {
+    let swiper_index = swiper_elements.length;
     swiper_elements.forEach((ele) => {
         let swiper = new Swiper(ele, {
             effect: "fade",
             fadeEffect: {
                 crossFade: true,
             },
-            autoplay: {
-                delay: 2000,
-                disableOnInteraction: true,
-            },
-
+            // autoplay: {
+            //     delay: 2000,
+            //     disableOnInteraction: true,
+            // },
             pagination: {
-                el: ".swiper-pagination",
+                el: `.pagination-${swiper_index}`,
               },
 
         });
+        swiper_index--;
     });
+
 }
